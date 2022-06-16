25301df03eec582d55e81777d6811da4842e10ff
krisee <krishiyana19@gmail.com>
2022-06-16 21:54:24 +0545

    database

--- src/index.js
+++ src/index.js
@@ -52,6 +52,41 @@ knex1.raw('CREATE DATABASE blogs_database3').then(function () {
     .then(function () {
       knex1.destroy();
     });
+}).catch(e=>{
+  console.log('error on database create');
+  // knex1.destroy();
+
+  // connect with database selected
+  connect.database = 'blogs_database3';
+  knex1 = knex({client : 'pg',
+  connection : connect,});
+
+  knex1.schema
+    .createTable('blogs', function (table) {
+      table.increments();
+      table.string('title').notNullable().unique();
+      table.string('description').nullable();
+      table.timestamps();
+    
+    })
+    .createTable('blogImages', function (table) {
+      table.increments();
+      table.bigInteger('blogId').notNullable();
+      table.string('image_url').nullable();
+      table.timestamps();
+    
+    })
+    .createTable('users', function (table) {
+      table.increments();
+      table.string('email').notNullable().unique();
+      table.string('name').nullable();
+      table.boolean('is_active').defaultTo(true);
+      table.timestamps();
+     
+    })
+    .then(function () {
+      knex1.destroy();
+    });
 });
 
 
